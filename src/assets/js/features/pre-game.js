9ffae8eb41586bbc02d5127385bffc08aaa3703b
thomas verlynde <thomas.verlynde@student.howest.be>
2022-03-29 14:38:35 +0200

    made amount of players in html and js put a limit on amount of players from 2 to 8 players

--- src/assets/js/features/pre-game.js
+++ src/assets/js/features/pre-game.js
@@ -1,8 +1,11 @@
 "use strict";
 let _nickname = null;
+let _amountPlayers = null;
 
 function showGames(e){
     e.preventDefault();
     _nickname = e.target.querySelector("#nickname").value;
     console.log(_nickname);
+    _amountPlayers = e.target.querySelector("#amount-players").value;
+    console.log(_amountPlayers);
 }
