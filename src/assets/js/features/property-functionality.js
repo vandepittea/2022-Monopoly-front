2ea99f960438e91b9038d8b1b9d8c5cc66613f7b
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2022-04-01 12:12:13 +0200

    Basic styling of the cards

--- src/assets/js/features/property-functionality.js
+++ src/assets/js/features/property-functionality.js
@@ -3,22 +3,22 @@
 function fillProperties()
 {
     const $propertiesContainer = document.querySelector("#properties-container");
-    const $railroadContainer = $propertiesContainer.querySelector("[data-streettype='railroad']");
-    const $utilitiesContainer = $propertiesContainer.querySelector("[data-streettype='utilities']");
+    const $railroadContainer = $propertiesContainer.querySelector("[data-streettype='railroad'] ul");
+    const $utilitiesContainer = $propertiesContainer.querySelector("[data-streettype='utilities'] ul");
 
     _tiles.forEach(tile =>
     {
         switch (tile.type)
         {
             case "street":
-                const $container = $propertiesContainer.querySelector(`[data-streettype='${tile.streetColor.toLowerCase()}']`);
-                $container.insertAdjacentHTML('beforeend', `<img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/>`);
+                const $container = $propertiesContainer.querySelector(`[data-streettype='${tile.streetColor.toLowerCase()}'] ul`);
+                $container.insertAdjacentHTML('beforeend', `<li><img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/></li>`);
                 break;
             case "railroad":
-                $railroadContainer.insertAdjacentHTML('beforeend', `<img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/>`);
+                $railroadContainer.insertAdjacentHTML('beforeend', `<li><img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/></li>`);
                 break;
             case "utility":
-                $utilitiesContainer.insertAdjacentHTML('beforeend', `<img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/>`);
+                $utilitiesContainer.insertAdjacentHTML('beforeend', `<li><img src="../images/tiles/${tile.nameAsPathParameter}.jpg" alt="${tile.name}"/></li>`);
                 break;
             default:
                 console.log("something else");
