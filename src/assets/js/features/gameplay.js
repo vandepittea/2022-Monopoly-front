9870604f488c11adcb9fa949e01a1166f1f6bb50
Aspen <aspen.eeckhout@gmail.com>
2022-04-18 11:42:49 +0200

    resolved merge issues

--- src/assets/js/features/gameplay.js
+++ src/assets/js/features/gameplay.js
@@ -77,6 +77,9 @@ function manageMainClick(e) {
             case "roll-dice":
                 rollDice();
                 break;
+            case "showAuctions":
+                currentAuctions();
+                break;
             case "main-property-buy":
                 buyProperty(e.target.closest("#main-tile-deed").dataset.name);
                 break;
@@ -97,4 +100,49 @@ function manageMainClick(e) {
                 break;
         }
     }
+}
+
+function currentAuctions() {
+    const $main = document.querySelector("main");
+    $main.innerText = "";
+    $main.insertAdjacentHTML("beforeend", _htmlElements.auctionTable);
+    fetchFromServer(url, 'GET')
+        .then(game => {
+            game.players.forEach(player => {
+                if (player.name !== _gameData.playerName) {
+                    //get all ongoing auctions by player
+                    fetchFromServer(`/games/${_gameData.gameID}/players/${player.name}/auctions`, 'GET')
+                        .then(response => {
+                            console.log(response);
+                            if (response.auctions.length > 0) {
+                                //show all ongoing auctions in a table on html
+                                const $auctionTableBody = $main.querySelector("#ongoingAuctions tbody");
+                                $auctionTableBody.insertAdjacentHTML("beforeend",
+                                    `<tr>
+                                            <td>${response.playerName}</td>
+                                            <td>${response.property}</td>
+                                            <td><img src="../images/coin.png" alt="Coin" title="Coin" id="coin"/>${response.price}</td>
+                                            <td>
+                                                <button id="joinAuction1" type="button">Join Auction</button>
+                                            </td>
+                                          </tr>`
+                                );
+                            }
+
+
+                        })
+                        .catch(errorHandler);
+                }
+            });
+        })
+        .catch(errorHandler);
+}
+
+function declareBankrupt() {
+
+    fetchFromServer(`/games/${_gameData.gameID}/players/${player.name}/bankruptcy', 'POST'`)
+        .then(response =>
+            console.log(response))
+
+    console.log(`${_gameData.playerName} is bankrupt!`);
 }
