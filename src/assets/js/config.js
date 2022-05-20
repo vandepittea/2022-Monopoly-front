294b12f8170366c9978bf8c1322dc9ffe83ba299
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2022-05-20 14:39:41 +0200

    Changed group number

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -2,7 +2,7 @@
 
 const _config = {
     prefix: "group17",
-    groupnumber: '00',
+    groupnumber: '17',
     errorHandlerSelector: '.errormessages p',
     getAPIUrl: function() { return `https://project-i.ti.howest.be/monopoly-${this.groupnumber}/api`;}
 };
