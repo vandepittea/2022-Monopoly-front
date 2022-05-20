b89a96c243fed3f3a07bd4ccb5f72db941fac9ee
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-20 11:18:06 +0200

    Fix bug in showing money of other players

--- src/assets/js/features/data-injectors.js
+++ src/assets/js/features/data-injectors.js
@@ -113,7 +113,7 @@ function showPlayerInfo(e) {
     $otherPlayerWindow.dataset.player = playerName;
     const player = getPlayerObject(_currentGameState, playerName);
     $otherPlayerWindow.querySelector("h2").innerText = player.name;
-    $otherPlayerWindow.querySelector("p").innerText = player.money;
+    $otherPlayerWindow.querySelector("h3").innerText = player.money;
 }
 
 function insertJailedMain($main, game) {
