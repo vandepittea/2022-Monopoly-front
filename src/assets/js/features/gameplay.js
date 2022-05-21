9a46521a3941f339651cd3905815fdd96054e054
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-21 23:34:18 +0200

    Optimalize manageGame

--- src/assets/js/features/gameplay.js
+++ src/assets/js/features/gameplay.js
@@ -2,47 +2,50 @@
 let _currentGameState = null;
 let _previousCyclePlayer = null;
 let _firstTimeCyclingManageGame = true;
+let _waitingTime = 3000;
 
 function manageGame() {
-    // TODO: delete this if-else, this exists for testing with dummy data
-    let url;
-    if (_gameData.token === null) {
-        url = '/games/dummy';
-    } else {
-        url = `/games/${_gameData.gameID}`;
-    }
-
-    fetchFromServer(url, 'GET')
+    fetchFromServer(`/games/${_gameData.gameID}`, 'GET')
         .then(game => {
             _currentGameState = game;
 
-            //All things that needs to be shown for both go here
-            injectProperties(game);
-            injectBalanceAndDebt(game);
+            showActiveAndOtherPlayers(game);
 
             if ((game.currentPlayer === _gameData.playerName) && (_previousCyclePlayer === _gameData.playerName || _firstTimeCyclingManageGame)) {
-                //All things specific for the active player goes here
-                injectPossibleTiles(game);
-                syncPlayersToMinimap(game);
-                fillActivePlayerMain(game);
+                showActivePlayer(game);
                 _firstTimeCyclingManageGame = false;
             } else {
-                //All things specific for non-active players go here
-                injectTopLeftTile(game);
-                fillOtherPlayerMain(game);
-                if (game.currentPlayer !== _previousCyclePlayer) {
-                    setTimeout(manageGame, calculateTimeout(game));
-                } else {
-                    setTimeout(manageGame, 3000);
-                }
-                _previousCyclePlayer = game.currentPlayer;
+                showAndDoActionsOtherPlayers(game);
             }
         })
         .catch(errorHandler);
 }
 
+function showActiveAndOtherPlayers(game){
+    injectProperties(game);
+    injectBalanceAndDebt(game);
+}
+
+function showActivePlayer(game){
+    injectPossibleTiles(game);
+    syncPlayersToMinimap(game);
+    fillActivePlayerMain(game);
+}
+
+function showAndDoActionsOtherPlayers(game){
+    injectTopLeftTile(game);
+    fillOtherPlayerMain(game);
+
+    if (game.currentPlayer !== _previousCyclePlayer) {
+        setTimeout(manageGame, calculateTimeout(game));
+    } else {
+        setTimeout(manageGame, _waitingTime);
+    }
+    _previousCyclePlayer = game.currentPlayer;
+}
+
 function calculateTimeout(game) {
-    return 5000 / game.numberOfPlayers;
+    return _waitingTime / game.numberOfPlayers;
 }
 
 function rollDice() {
@@ -60,7 +63,7 @@ function rollDice() {
                     $main.innerText = "";
                     injectTurnInMain(getLastTurn(response), $main);
 
-                    setTimeout(manageGame, 2000);
+                    setTimeout(manageGame, _waitingTime);
 
                     _currentGameState = response;
                 })
